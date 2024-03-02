fb60bdf3e28dc5dd7b896102699cd36d91e71d00
zharuk.alex <zzharuk@gmail.com>
2024-03-02 19:48:40 +0200

    Initial dev

---
+++ src/README.md
@@ -0,0 +1,3 @@
+### goit-markup-hw-03
+
+[preview](https://zharuk-alex.github.io/goit-markup-hw-03/)
---
+++ vite.config.js
@@ -0,0 +1,62 @@
+import { defineConfig } from "vite";
+import * as glob from 'glob';
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+
+export default defineConfig(({ command }) => {
+  return {
+    base: './',
+    define: {
+      [command === "serve" ? "global" : "_global"]: {},
+    },
+    root: "src",
+    filenameHashing: false,
+    build: {
+      cssCodeSplit: true,
+      minify: false, 
+      css: {
+        minify: false
+      },
+      sourcemap: false,
+
+      rollupOptions: {
+        input: glob.sync("./src/*.html"),
+        output: {
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name.endsWith('.css')) {
+              return 'css/[name][extname]';
+            }
+            
+            if (assetInfo.name.endsWith('.jpg') || assetInfo.name.endsWith('.png')) {
+              const folder = assetInfo.name?.split("_")?.[0]
+              if (folder?.length) {
+                return `images/${folder}/[name][extname]`; 
+              }
+            }
+            return 'assets/[name][extname]';
+          },
+        },
+      },
+      outDir: "../dist",
+    },
+  
+    plugins: [
+      injectHTML(),
+      FullReload("./src/**/**.html"),
+      ViteImageOptimizer({
+        png: {
+          quality: 86,
+        },
+        jpeg: {
+          quality: 86,
+        },
+        jpg: {
+          quality: 86,
+        },
+      }),
+      
+    ],
+  };
+});
