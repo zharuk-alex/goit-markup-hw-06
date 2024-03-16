2460107f6b80733951f6bcc5ef14d4e886accf40
zharuk.alex <zzharuk@gmail.com>
2024-03-16 18:37:47 +0200

    media-queries added

--- vite.config.js
+++ vite.config.js
@@ -1,67 +1,67 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import * as glob from 'glob';
-import injectHTML from "vite-plugin-html-inject";
-import FullReload from "vite-plugin-full-reload";
-import imageOptimizerConfig from "./vite-plugin-image-optimizer"
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-
+import injectHTML from 'vite-plugin-html-inject';
+import imageOptimizerConfig from './vite-plugin-image-optimizer';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(({ command }) => {
   return {
     base: './',
     define: {
-      [command === "serve" ? "global" : "_global"]: {},
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-    root: "src",
+    root: 'src',
     filenameHashing: false,
     build: {
+      emptyOutDir: true,
       cssCodeSplit: true,
-      minify: false, 
+      minify: false,
       sourcemap: false,
+      outDir: '../dist',
       rollupOptions: {
-        emptyOutDir: true,
-        input: glob.sync("./src/*.html"),
+        input: glob.sync('./src/*.html'),
         output: {
-          assetFileNames: (assetInfo) => {
+          assetFileNames: assetInfo => {
             if (assetInfo.name.endsWith('.css')) {
               return 'css/[name][extname]';
             }
-            
-            if (assetInfo.name.endsWith('.jpg') || assetInfo.name.endsWith('.png')) {
-              const folder = assetInfo.name?.split("_")?.[0]
+
+            if (
+              assetInfo.name.endsWith('.jpg') ||
+              assetInfo.name.endsWith('.png')
+            ) {
+              const folder = assetInfo.name?.split('_')?.[0];
               if (folder?.length) {
-                return `images/${folder}/[name][extname]`; 
+                return `images/${folder}/[name][extname]`;
               }
             }
 
-            if (assetInfo.name == "icons.svg") {
+            if (assetInfo.name == 'icons.svg') {
               return 'images/[name][extname]';
             }
-            
+
             return 'assets/[name][extname]';
           },
         },
       },
-      outDir: "../dist",
     },
-  
+
     plugins: [
       injectHTML(),
-      FullReload("./src/**/**.html"),
       ViteImageOptimizer(imageOptimizerConfig),
       viteStaticCopy({
-      targets: [
+        targets: [
           {
             src: 'js/*.js',
-            dest: 'js'
+            dest: 'js',
           },
           // {
           //   src: 'images/*',
           //   dest: 'images'
           // },
-        ]
-      })
+        ],
+      }),
     ],
   };
 });
