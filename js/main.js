e8d3d47492752d1117b3cc473b50f7f5d1ae3509
zharuk.alex <zzharuk@gmail.com>
2024-03-16 18:46:43 +0200

    media queries added

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 const navigationActiveToggle = () => {
-    const currentPage = window.location.pathname.split("/").pop(); 
-    const navLinks = document.querySelectorAll(".nav-link") ?? []; 
-    const isIndex = window.location.pathname === '/' || currentPage === "index.html" || currentPage?.length == 0;
-    
-    if (isIndex) {
-        return false;
-    }
+  const currentPage = window.location.pathname.split('/').pop();
+  const navLinks = document.querySelectorAll('.nav-link') ?? [];
+  const isIndex =
+    window.location.pathname === '/' ||
+    currentPage === 'index.html' ||
+    currentPage?.length == 0;
 
-    navLinks.forEach(link => {
-        link.classList.remove("active");
-        if (link.getAttribute("data-href") === currentPage){
-            link.classList.add("active");
-        }
-    });
-}
+  if (isIndex) {
+    return false;
+  }
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('data-href') === currentPage) {
+      link.classList.add('active');
+    }
+  });
+};
 
 (() => {
-    navigationActiveToggle();
+  navigationActiveToggle();
 })();
